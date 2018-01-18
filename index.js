dae1d44070be3e72faa380614297800574b3c5d2
Danish <danish8802204230@gmail.com>
2018-01-18 14:10:24 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,65 @@
+module.exports = class {
+    constructor(type) {
+        this.type = type
+        this.queries = []
+    }
+
+    addQuery(obj) {
+        let nodes = '', args = ''
+
+        if (obj.nodes) {
+            Object.keys(obj.nodes).map((key) => {
+                let node = obj.nodes[key]
+                if (typeof node === 'object') {
+                    nodes += this.addQuery({
+                        nodes: node,
+                        name: key,
+                        returnNode: true,
+                    })
+                } else {
+                    nodes += ` ${key} `
+                }
+            })
+        }
+
+        if (obj.args) {
+            Object.keys(obj.args).map((key) => {
+                let arg = obj.args[key]
+                if (typeof arg === 'object') {
+                    args += `${this.addQuery({
+                        args: arg,
+                        returnArgs: true,
+                        name: key
+                    })}`
+                } else {
+                    let qoutes = ''
+                    if (typeof arg == 'string')
+                        qoutes = '"'
+                    args += ` ${key}: ${qoutes}${arg}${qoutes} `
+                }
+            })
+        }
+
+        if (obj.args && Object.keys(obj.args).length > 0 && !obj.returnArgs) {
+            args = `(${args})`
+        }
+
+        if (obj.returnNode) {
+            return `${obj.name}{ 
+                ${nodes}   
+            }`
+        } else if (obj.returnArgs) {
+            return `${obj.name}: {${args}}`
+        } else {
+            this.queries.push(`${obj.name} ${args} { 
+                ${nodes}   
+            }`)
+        }
+    }
+
+    getGraphQLString() {
+        return `${this.type ? this.type : ''}{
+            ${this.queries.join(' ')}
+        }`
+    }
+}
